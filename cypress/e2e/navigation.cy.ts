1d81868b08d13fc95eb8e64abdb5727ef56350d7
AlexGopal <127698347+AlexGopal@users.noreply.github.com>
2024-07-10 00:10:44 -0400

    Attempted to fix sidebar navigation button but the cypress test times out (#5)
    * Attempted to fix sidebar navigation button but the cypress test times out
    
    * removed the failed e2e testing to try and merge
    
    ---------
    
    Co-authored-by: Alex Gopal <alexgopal2@gmail.com>

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -91,5 +91,125 @@ describe("Sidebar Navigation", () => {
       cy.wait(500);
       isNotInViewport("nav");
     });
+
+    // it("support button works and opens email", () => {
+    //   // Stub window.location.href
+    //   cy.window().then((win) => {
+    //     cy.stub(win, "location").as("windowLocation");
+    //   });
+
+    //   // Click the Support button
+    //   cy.get("nav").contains("Support").click();
+
+    //   // Verify the mailto link
+    //   const email = "support@prolog-app.com";
+    //   const subject = "Support Request";
+    //   const body = "Please Describe your issue";
+    //   const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    //   cy.get("@windowLocation").should("have.property", "href", mailtoLink);
+    // });
+
+    // this test uses the error_screenshot2 and it fails because the test timesout since it cant tell that the new window is opened
+    // google says its a limitation of cypress so need to find another way to do the test
+
+    //     it('has correct mail to link on the support button', () => {
+    //       const email = "support@prolog-app.com";
+    //       const subject = "Support Request";
+    //       const body = "Please Describe your issue";
+    //       const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    //       cy.get('button').contains('Support')
+    //           .should("have.property", "href", mailtoLink)
+    //           // yields the value of the "href" attribute
+    //           .should('match', /^mailto:/)
+    //           // parse the URL into recipient, subject, body fields
+    //           .should('deep.equal', {
+    //               recipient: "support@prolog-app.com",
+    //               subject: "Support Request",
+    //               body: "Please Describe your issue",
+    //           })
+    //      });
+
+    // //   // this test uses the error_screenshot and it fails because the button has no property href
   });
 });
+
+// can just scroll up
+
+// // eslint-disable-next-line @typescript-eslint/no-empty-function
+// describe("Sidebar Navigation", () => {
+//   beforeEach(() => {
+//     cy.viewport(1025, 900);
+//     cy.visit("http://localhost:3000/dashboard");
+//   });
+//   it("links are working", () => {
+//     cy.get("nav").contains("Issues").click();
+//     cy.url().should("eq", "http://localhost:3000/dashboard/issues");
+//     cy.get("nav").contains("Projects").click();
+//     cy.url().should("eq", "http://localhost:3000/dashboard");
+//   });
+
+//   it("is collapsible", () => {
+//     // collapse navigation
+//     cy.get("nav").contains("Collapse").click();
+//     // check that links still exist
+//     cy.get("nav").find("a").should("have.length", 5).eq(1).click();
+//     cy.url().should("eq", "http://localhost:3000/dashboard/issues");
+//     cy.get("nav").contains("Issues").should("not.exist");
+//   });
+
+//   it("support button works and opens email", () => {
+//     // Stub window.location.href
+//     cy.window().then((win) => {
+//       cy.stub(win, "location").as("windowLocation");
+//     });
+
+//     // Click the Support button
+//     cy.get("nav").contains("Support").click();
+
+//     // Verify the mailto link
+//     const email = "support@prolog-app.com";
+//     const subject = "Support Request";
+//     const body = "Please Describe your issue";
+//     const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+//     cy.get("@windowLocation").should("have.property", "href", mailtoLink);
+//   });
+
+//   // this test uses the error_screenshot2 and it fails because the test timesout since it cant tell that the new window is opened
+//   // google says its a limitation of cypress so need to find another way to do the test
+
+//   // it('has correct mailto link on the support button', () => {
+//   //     // Spy on window.location.href
+//   //     cy.window().then((win) => {
+//   //       cy.spy(win, 'assign').as('windowAssign');
+//   //     });
+
+//   //     // Click on the support button
+//   //     cy.get('button').contains('Support').click();
+
+//   //     // Check if the correct mailto: URL was called
+//   //     cy.get('@windowAssign').should('be.calledWith', 'mailto:support@prolog-app.com?subject=Support%20Request&body=Please%20Describe%20your%20issue');
+//   //   });
+
+//   // ignore this one
+
+//   //   it('has correct mailto link on the support button', () => {
+//   //     const email = "support@prolog-app.com";
+//   //     const subject = "Support Request";
+//   //     const body = "Please Describe your issue";
+//   //     const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+//   //     cy.get('button').contains('Support')
+//   //         .should("have.property", "href", mailtoLink)
+//   //         // yields the value of the "href" attribute
+//   //         .should('match', /^mailto:/)
+//   //         // parse the URL into recipient, subject, body fields
+//   //         .should('deep.equal', {
+//   //             recipient: "support@prolog-app.com",
+//   //             subject: "Support Request",
+//   //             body: "Please Describe your issue",
+//   //         })
+//   //   });
+
+//   // this test uses the error_screenshot and it fails because the button has no property href
+// });
