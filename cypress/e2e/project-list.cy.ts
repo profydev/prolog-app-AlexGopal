4d784ab1fb13d1414ab6e5b913c2b635e012ac16
AlexGopal <127698347+AlexGopal@users.noreply.github.com>
2024-08-22 22:45:18 -0400

    fixed the project list showing the incorrect status and made the project badge match the status. Also wrote a cypress test to test this. (#11)
    Co-authored-by: Alex Gopal <alexgopal2@gmail.com>

--- cypress/e2e/project-list.cy.ts
+++ cypress/e2e/project-list.cy.ts
@@ -23,6 +23,12 @@ describe("Project List", () => {
     it("renders the projects", () => {
       const languageNames = ["React", "Node.js", "Python"];
 
+      const statusMap: { [key: string]: string } = {
+        error: "critical",
+        warning: "warning",
+        info: "stable",
+      };
+
       // get all project cards
       cy.get("main")
         .find("li")
@@ -32,10 +38,44 @@ describe("Project List", () => {
           cy.wrap($el).contains(languageNames[index]);
           cy.wrap($el).contains(mockProjects[index].numIssues);
           cy.wrap($el).contains(mockProjects[index].numEvents24h);
-          cy.wrap($el).contains(capitalize(mockProjects[index].status));
+          cy.wrap($el).contains(
+            capitalize(statusMap[mockProjects[index].status]),
+          );
+          //<Badge color={statusColors[statusMap[status]]}>{capitalize(statusMap[status])}
           cy.wrap($el)
             .find("a")
             .should("have.attr", "href", "/dashboard/issues");
+        });
+    });
+
+    it("checks if the status and badge color are set correctly", () => {
+      const statusMap: { [key: string]: string } = {
+        error: "critical",
+        warning: "warning",
+        info: "stable",
+      };
+
+      const statusColors: { [key: string]: string } = {
+        critical: "rgb(254, 243, 242)", // Error: #fef3f2 in RGB
+        warning: "rgb(255, 250, 235)", // Warning: #fffaeb in RGB
+        stable: "rgb(236, 253, 243)", // Info: #ecfdf3 in RGB
+      };
+
+      cy.get("main")
+        .find("li")
+        .find(".badge_container__FVLnl")
+        .each(($el, index) => {
+          const status = mockProjects[index].status;
+          const expectedStatus = statusMap[status];
+          const expectedColor = statusColors[expectedStatus];
+
+          console.log("Status:", status);
+          console.log("expectedStatus:", expectedStatus);
+          console.log("expectedColor:", expectedColor);
+
+          cy.wrap($el)
+            .should("have.text", capitalize(expectedStatus))
+            .and("have.css", "background-color", expectedColor);
         });
     });
   });
