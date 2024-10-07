4b7700415c8b38bc85c1b0948c8d58e1a99772aa
AlexGopal <127698347+AlexGopal@users.noreply.github.com>
2024-10-07 00:26:36 -0400

    Projects error screen (#16)
    * added the error screen to projetlist for both desktop and mobile versions.
    
    * made it so when the try again button is clicked, the data on the page reloads.
    
    * added cypress test to make sure the error message is displaying correctly.
    
    * fixed alert container on high screen sizes
    
    * fixed cypress test to make sure the error message is displaying correctly.
    
    ---------
    
    Co-authored-by: Alex Gopal <alexgopal2@gmail.com>

--- features/projects/api/use-get-projects.tsx
+++ features/projects/api/use-get-projects.tsx
@@ -1,7 +1,20 @@
+// import { useQuery } from "@tanstack/react-query";
+// import { getProjects } from "@api/projects";
+// import type { Project } from "@api/projects.types";
+
+// export function useGetProjects() {
+//   return useQuery<Project[], Error>(["projects"], getProjects);
+// }
+
 import { useQuery } from "@tanstack/react-query";
 import { getProjects } from "@api/projects";
 import type { Project } from "@api/projects.types";
 
 export function useGetProjects() {
-  return useQuery<Project[], Error>(["projects"], getProjects);
+  const query = useQuery<Project[], Error>(["projects"], getProjects);
+
+  return {
+    ...query,
+    refetch: query.refetch,
+  };
 }
--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -1,9 +1,10 @@
 import { ProjectCard } from "../project-card";
 import { useGetProjects } from "../../api/use-get-projects";
 import styles from "./project-list.module.scss";
+import { Alert } from "@features/ui";
 
 export function ProjectList() {
-  const { data, isLoading, isError, error } = useGetProjects();
+  const { data, isLoading, isError, error, refetch } = useGetProjects();
 
   if (isLoading) {
     return (
@@ -15,7 +16,15 @@ export function ProjectList() {
 
   if (isError) {
     console.error(error);
-    return <div>Error: {error.message}</div>;
+    // return <div>Error: {error.message}</div>;
+    // return <div><Alert>Error: {error.message}</Alert></div>;
+    return (
+      <div>
+        <Alert onActionClick={refetch}>
+          There was a problem while loading the project data.
+        </Alert>
+      </div>
+    );
   }
 
   return (
