8b2c3a6dfdf7d3c9e5c4b7f7f1205d29a79c941e
Alex Gopal <alexgopal2@gmail.com>
2024-10-14 04:48:14 -0400

    fixed bug where badge color was off by one of it should be (in rgb)

--- features/ui/new_button/new-button.tsx
+++ features/ui/new_button/new-button.tsx
@@ -1,54 +1,5 @@
-// import { ButtonHTMLAttributes } from "react";
 import React from "react";
-// import classNames from "classnames";
 import styles from "./new-button.module.scss";
-
-// export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
-//   return (
-//     <button {...props} className={classNames(styles.button, props.className)} />
-//   );
-// }
-
-// export enum ButtonSize {
-//   sm = "sm",
-//   md = "md",
-//   lg = "lg",
-// }
-
-// export enum ButtonColor {
-//   primary = "primary",
-//   gray = "gray",
-//   error = "error",
-//   warning = "warning",
-//   success = "success",
-// }
-
-// export enum ButtonIcon {
-//   before = "before",
-//   trialing = "trialing",
-//   only = "only",
-// }
-
-// type ButtonProps = {
-//   size: ButtonSize;
-//   color: ButtonColor;
-//   icon?: ButtonIcon;
-// }
-
-// export function Button({color, size, icon}: ButtonProps) {
-//   return (
-//     <div>
-//       <button className = {classNames(styles.container, styles[size], styles[color])}>
-//         <img src={"/icons/circle.svg"} alt="icon" className={styles.icon} />
-//         <span>Button CTA</span>
-//       </button>
-//     </div>
-//   )
-// }
-
-// Button Component (button.tsx)
-
-// Button Component (button.tsx)
 
 export type ButtonSize = "sm" | "md" | "lg" | "xlg";
 export type ButtonColor =
@@ -71,20 +22,6 @@ type ButtonProps = {
   disabled?: boolean;
   onClick?: () => void;
 };
-
-// export function Button({ size = 'md', color = 'primary', iconPosition = 'before', onClick }: ButtonProps) {
-//   return (
-//     <button
-//       className={`${styles.button} ${styles[size]} ${styles[color]} ${styles[iconPosition]}`}
-//       onClick={onClick}
-//     >
-//       {iconPosition === 'before' && <img src="/icons/icon.svg" alt="icon" className={styles.icon} />}
-//       {iconPosition !== 'only' && <span>Button CTA</span>}
-//       {iconPosition === 'after' && <img src="/icons/icon.svg" alt="icon" className={styles.icon} />}
-//       {iconPosition === 'only' && <img src="/icons/icon.svg" alt="icon" className={styles.icon} />}
-//     </button>
-//   );
-// }
 
 export function NewButton({
   size = "md",
