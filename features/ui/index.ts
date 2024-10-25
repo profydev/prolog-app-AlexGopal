6b51785a0911fd89707b1799955ed8042075b477
Alex Gopal <alexgopal2@gmail.com>
2024-10-25 02:42:40 -0400

    added a barebones input component as well as the index file, also added the index file for select.

--- features/ui/index.ts
+++ features/ui/index.ts
@@ -1,3 +1,5 @@
 export * from "./badge";
 export * from "./button";
 export * from "./alert";
+export * from "./select";
+export * from "./input";
--- features/ui/select/select-component.tsx
+++ features/ui/select/select-component.tsx
@@ -1,31 +1,35 @@
 import React, { useState } from "react";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Select, { SelectChangeEvent, SelectProps } from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import styles from "./select-component.module.scss";
 
-type SelectProps = Record<string, never>;
+type CustomSelectProps = SelectProps & {
+  label?: string;
+};
 
-export function SelectComponent(props: SelectProps) {
+export function SelectComponent({
+  label = "Age",
+  ...props
+}: CustomSelectProps) {
   const [age, setAge] = useState<string | number>("");
 
-  const handleChange = (event: SelectChangeEvent<string | number>) => {
-    setAge(event.target.value as number); // Update state with the selected value
+  const handleChange = (event: SelectChangeEvent<unknown>) => {
+    setAge(event.target.value as string | number); // Cast value to string | number
   };
 
   return (
-    <div className={styles.container} {...props}>
-      {" "}
-      {/* Apply container styles here */}
+    <div className={styles.container}>
       <FormControl sx={{ width: "15%" }}>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           value={age}
-          label="Age"
           onChange={handleChange}
+          label={label}
+          {...props}
         >
           <MenuItem value="">
             <em>None</em>
